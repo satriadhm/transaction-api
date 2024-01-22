96eb0e6d4f346c15346446b8dc65b311e85d509f
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2024-01-22 09:48:18 +0700

    tes asisten praktikum

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using order_api.Model;
+using System.Diagnostics;
+
+namespace order_api.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController
+    {
+        public static List<Transaction> transactions = new List<Transaction>
+        {
+            new Transaction(1, "Toys", "3 kinds", "utils", "Toys transaction to be ordered."),
+            new Transaction(2, "Utensils", "3 kinds", "utils", "Utensil transaction to be ordered.")
+        };
+
+        [HttpGet]
+        public IEnumerable<Transaction> Get()
+        {
+            Debug.Assert(transactions != null, "Transaction data should not be null");
+            return transactions;
+        }
+
+        [HttpGet("{id}")]
+        public Transaction? Get(int id) 
+        {
+            Debug.Assert(id > 0, "Id should be greater than 0");
+            for (int i = 0; i < transactions.Count; i++) 
+            {
+                if (transactions[i].Id == id) 
+                {
+                    return transactions[i];
+                }
+            }
+            return null;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Transaction transaction) 
+        {
+            Debug.Assert(transaction != null, "Transaction should not be null");
+            transactions.Add(transaction);
+        }
+
+        [HttpPut]
+        public void Put(int id,[FromBody] Transaction transaction) 
+        {
+            for (int i = 0; i < transactions.Count; i++) 
+            {
+                if (transactions[i].Id == id) 
+                {
+                    transactions[i] = transaction;
+                }
+            }
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            for (int i = 0; i < transactions.Count; i++) 
+            {
+                if (transactions[i].Id == id) 
+                {
+                    transactions.RemoveAt(i);
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ Model/Transaction.cs
@@ -0,0 +1,22 @@
+﻿namespace order_api.Model
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+
+        public Transaction(int id, string name, string value, string type, string description)
+        {
+            Id = id;
+            Name = name;
+            Value = value;
+            Type = type;
+            Description = description;
+        }
+
+
+    }
+}
